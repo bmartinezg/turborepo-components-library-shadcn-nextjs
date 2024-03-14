c46b0ec8981d8fbb90c0379b79cd063ffc31d9ca
Bruno Martínez González <bruno.martinez@new-work.se>
2024-03-14 14:30:21 +0100

    all working

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@turbo/ui/styles.css";
+import "@turbo/tscn/styles.css";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
